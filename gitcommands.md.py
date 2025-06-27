2128c104d59acdbafb26921938cc2500645f10d2
Ruben <rubennnovoa@gmail.com>
2025-06-27 19:33:57 -0300

    add gitcommands.md
    este va a ser el cheat sheet de los comandos de git vistos en el curso

---
+++ gitcommands.md.py
@@ -0,0 +1,2 @@
+git init: "inicializa un repositorio en la compu"
+git commit: "sincroniza la información del repositorio local con el repositorio en la nube"
---
+++ misdatos.py
@@ -0,0 +1,3 @@
+nombre = "Rubén"
+apellido = "Novoa"
+edad = 63
